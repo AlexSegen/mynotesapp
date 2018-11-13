75a03aa135246c31067bb09c8710722ab7e963a8
Alejandro Vivas <alejandrojv17@gmail.com>
2018-11-13 11:36:22 -0300

    string template en servicios

--- src/services/category.services.js
+++ src/services/category.services.js
@@ -6,15 +6,15 @@ export default {
         return axios.get(RESOURCE_NAME);
     },
     get(id) {
-        return axios.get(RESOURCE_NAME + '/' + id);
+        return axios.get(`${RESOURCE_NAME}/${id}`);
     },
     post(data) {
         return axios.post(RESOURCE_NAME, data);
     },
     put(data) {
-        return axios.put(RESOURCE_NAME + '/' + data.id, data);
+        return axios.put(`${RESOURCE_NAME}/${data.id}`, data);
     },
     delete(id) {
-        return axios.delete(RESOURCE_NAME + '/' + id);
+        return axios.delete(`${RESOURCE_NAME}/${id}`);
     }
 }
